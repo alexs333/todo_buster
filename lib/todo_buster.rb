740b4ca73118bb504851ab9c4e09385df13d3bfd
Alex S <aserdyuk@dius.com.au>
2016-01-11 12:22:59 +1100

    Implemented finder

--- lib/todo_buster.rb
+++ lib/todo_buster.rb
@@ -1,5 +1,22 @@
-require "todo_buster/version"
+require 'todo_buster/version'
 
-module TodoBuster
-  # Your code goes here...
+class TodoBuster::Finder
+  def find_todos
+    puts "currently in directory: #{Dir.pwd}"
+
+    todos = []
+
+    Dir.glob("#{Dir.pwd}/**/*.rb").each do |test|
+      todos << open(test).each_line.with_index(1).inject([]) do |m, i|
+        m << i if (i[0][/TODO/])
+        m
+      end
+    end
+
+    # puts todos[0]
+    # puts todos[1]
+
+    #  git blame -L 1,1 Vagrantfile
+    puts todos[2]
+  end
 end
